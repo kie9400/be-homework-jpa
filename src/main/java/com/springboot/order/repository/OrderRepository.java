8dc534b709b4c1525bdfaca515e0e209aec2adad
hogeun <166356000+kie9400@users.noreply.github.com>
2025-01-16 17:12:40 +0900

    Delete src directory

--- src/main/java/com/springboot/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.order.repository;
-
-import com.springboot.order.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> { // 수정된 부분
-}
